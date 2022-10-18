4fceb99782871c6098bb8acdad4e13ba5e8407dd
Mariana Lima <77506074+Marianalima31@users.noreply.github.com>
2022-10-18 08:16:19 -0300

    v2

--- src/src/main/java/com/example/jwt/repository/RoleRepository.java
+++ src/src/main/java/com/example/jwt/repository/RoleRepository.java
@@ -1,14 +1,14 @@
 package com.example.jwt.repository;
 
-import com.example.jwt.model.UserModel;
+import com.example.jwt.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, String> {
+public interface RoleRepository extends JpaRepository<RoleModel, Long> {
 
-    Optional<UserModel> findByUsername(String username);
+    Optional<RoleModel> findByName(String name);
 
 }
